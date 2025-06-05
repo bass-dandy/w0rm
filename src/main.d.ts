a3a2c8cd849d5c86f4401bf6f6774d2d686bde91
bass-dandy <ctdinh93@gmail.com>
2025-06-05 02:48:53 -0400

    add types for ts consumers

---
+++ src/main.d.ts
@@ -0,0 +1,18 @@
+declare class Game {
+	constructor(
+		canvas: HTMLCanvasElement,
+		onScoreUpdate?: (score: number) => void
+	);
+
+	enableKeyboardControls: void;
+	disableKeyboardControls: void;
+	play: void;
+	pause: void;
+	up: void;
+	right: void;
+	down: void;
+	left: void;
+	fire1: void;
+	fire2: void;
+	destroy: void;
+}
