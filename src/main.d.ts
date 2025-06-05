3ad6ebb2822578fa30a693ef95b663d00d847f95
bass-dandy <ctdinh93@gmail.com>
2025-06-05 03:18:25 -0400

    fix type definitions one more time

--- src/main.d.ts
+++ src/main.d.ts
@@ -4,15 +4,15 @@ export default class Game {
 		onScoreUpdate?: (score: number) => void
 	);
 
-	enableKeyboardControls: void;
-	disableKeyboardControls: void;
-	play: void;
-	pause: void;
-	up: void;
-	right: void;
-	down: void;
-	left: void;
-	fire1: void;
-	fire2: void;
-	destroy: void;
+	enableKeyboardControls(): void;
+	disableKeyboardControls(): void;
+	play(): void;
+	pause(): void;
+	up(): void;
+	right(): void;
+	down(): void;
+	left(): void;
+	fire1(): void;
+	fire2(): void;
+	destroy(): void;
 }
